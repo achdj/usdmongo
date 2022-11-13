0e2e88608111a7fabd715b2ed50892a858e69e32
achdj <achilledjegnon@gmail.com>
2022-11-13 22:54:35 +0000

    add many model of people

--- myApp.js
+++ myApp.js
@@ -1,28 +1,43 @@
 require('dotenv').config();
 
 let mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+let Schema = mongoose.Schema;
 
-let Person;
+var Person;
 
 //new_user_mongP
 //qLIoh3DUe9ek5ct6
-mongoose.connect('mongodb+srv://new_user_mongP:qLIoh3DUe9ek5ct6@cluster0.dveiqld.mongodb.net/fcc-mongodb-and-mongoose?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+/** Create a 'Person' Model */
 const personSchema = new Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String]
 });
 
+/** Create and Save a Person */
 Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var user = new Person({name:"achille", age:22, favoriteFoods: ["Haricot, Pain"]});
+  user.save(function(err, data) {
+    if(err) return console.log(err);
+    done(null, data);
+  });
 };
 
+/** Create many People with `Model.create()` */
+arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function(err, people) {
+    if(err) return console.log(err);
+    done(null, people);
+  })
 };
 
 const findPeopleByName = (personName, done) => {
